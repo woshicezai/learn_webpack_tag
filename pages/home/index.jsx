7d6302205d5a626f083c0e17da5edbc92cbca1ff
zhouce <zhouce@hualala.com>
2020-06-20 12:15:44 +0800

    fix 箭头编译失败

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 /**
  * 首页
  */
-export default class Home  extends React.Component{
+export default class Home extends React.Component {
+  onError = () => {
+    throw new Error('出错啦');
+  }
+
   render() {
     return (
-      <div>{'这里是首页'}</div>
+      <div>
+        <div>{'这里是首页'}</div>
+        <button onClick={this.onError}>{'点击我试试 会出错哦'}</button>
+      </div>
     )
   }
 }
